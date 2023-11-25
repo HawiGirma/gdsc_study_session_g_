f8b8aab4671936a7a4d0082561f2800714e627a5
Hawi Girma <hawigirmamegersag@gmail.com>
2023-11-25 15:21:14 +0300

    I have worked on it and commit it now

--- Dart-Tasks/task-2.dart
+++ Dart-Tasks/task-2.dart
@@ -1,1 +1,41 @@
+void main() {
+  List<int> numbers = [75, 80, 85, 90, 95];
 
+  print('Maximum number: ${findMaximum(numbers)}');
+  print('Minimum number: ${findMinimum(numbers)}');
+  print('Sum of all numbers: ${calculateSum(numbers)}');
+  print('Average of all numbers: ${calculateAverage(numbers)}');
+}
+
+int findMaximum(List<int> numbers) {
+  int max = numbers[0];
+  for (int number in numbers) {
+    if (number > max) {
+      max = number;
+    }
+  }
+  return max;
+}
+
+int findMinimum(List<int> numbers) {
+  int min = numbers[0];
+  for (int number in numbers) {
+    if (number < min) {
+      min = number;
+    }
+  }
+  return min;
+}
+
+int calculateSum(List<int> numbers) {
+  int sum = 0;
+  for (int number in numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+double calculateAverage(List<int> numbers) {
+  int sum = calculateSum(numbers);
+  return sum / numbers.length;
+}
